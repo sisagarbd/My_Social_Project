2af0aeb6d0c7485d28ff700ded18684974a6798c
sisagarbd <46032500+sisagarbd@users.noreply.github.com>
2023-09-22 13:54:43 +0600

    From & view add for login

---
+++ App_Login/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CreateNewUser(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password1', 'password2')
+        
