422c39dc145c2f4f1288e14d6e79bdc4fd7f1328
sisagarbd <46032500+sisagarbd@users.noreply.github.com>
2023-10-02 02:10:02 +0600

    Create login form in my social media

--- App_Login/views.py
+++ App_Login/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, HttpResponseRedirect
 from App_Login.forms  import CreateNewUser
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse, reverse_lazy
+from App_Login.models import UserProfile
+from django.contrib.auth.forms import AuthenticationForm
+
+
 
 def sign_up(request):
     form = CreateNewUser()
@@ -11,6 +15,23 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             registered = True
-            pass
+            user_profiel = UserProfile(user=user)
+            return HttpResponseRedirect(reverse('App_Login:login'))
 
-    return render(request, 'App_Login/sign_up.html', {'title': 'Sign Up . Instragram', 'form': form})
+    return render(request, 'App_Login/sign_up.html',context= {'title': 'Sign Up . Instragram', 'form': form})
+
+
+def login_page(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                pass
+
+    return render(request, 'App_Login/login.html', context={'title':'Login', 'form':form})
